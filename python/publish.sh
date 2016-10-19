4da437075c8f9c81989e9aeedfc34c17bcaef1ee
Russell Hay <me@russellhay.com>
2016-10-19 13:07:55 -0700

    adding python configuration and fixing license

---
+++ python/publish.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Make sure you pip install twine
+
+set -e
+
+rm -rf dist
+python setup.py sdist
+python setup.py bdist_wheel
+python3 setup.py bdist_wheel
+twine upload dist/*
---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tc16',
+    version='1.0.2',
+    author='Tableau',
+    author_email='github@tableau.com'
+    url='https://github.com/tableau/tc16',
+    packages=find_packages(),
+    license='MIT',
+    description='Make TC16 rad.'
+)
