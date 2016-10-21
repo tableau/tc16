50ff39c322fd9f00de1a47d68781770d6c25ea23
Jared Dominguez <jred51@gmail.com>
2016-10-21 16:07:26 -0700

    Make postinstall message cross-platform

--- node/shared/post_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "******************** READ ME - HOW TO USE THIS  ************************"
-echo "Using:    type 'tc16' into your terminal or command prompt (no quotes)"
-echo "Info:     tc16 is a simple command line interface for downloading"
-echo "          Tableau's developer tools and content."
-echo "************************************************************************"
