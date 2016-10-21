7fc09eb3021a6305dd456648b15e97b4b4ec0014
lbrendanl <lee33@uw.edu>
2016-10-21 10:09:31 -0700

    Added post install script to nom to explain tool.

---
+++ node/post_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "******************** READ ME - HOW TO USE THIS  ************************"
+echo "Using:    type 'tc16' into your terminal or command prompt (no quotes)"
+echo "Info:     tc16 is a simple command line interface for downloading"
+echo "          Tableau's developer tools and content."
+echo "************************************************************************"
